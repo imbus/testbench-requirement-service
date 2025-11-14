f086328a48830f3dd6b0a7599d5723ac4aabb7d3
Linddrit Aliu <linddrit.aliu@imbus.de>
2025-11-14 09:57:14 +0100

    Reorganize reader classes into separate folders

--- src/testbench_requirement_service/readers/__init__.py
+++ src/testbench_requirement_service/readers/__init__.py
@@ -1,7 +1,7 @@
 from testbench_requirement_service.readers.abstract_reader import AbstractRequirementReader
-from testbench_requirement_service.readers.excel_reader import ExcelRequirementReader
-from testbench_requirement_service.readers.jira_reader import JiraRequirementReader
-from testbench_requirement_service.readers.jsonl_reader import JsonlRequirementReader
+from testbench_requirement_service.readers.excel.reader import ExcelRequirementReader
+from testbench_requirement_service.readers.jira.reader import JiraRequirementReader
+from testbench_requirement_service.readers.jsonl.reader import JsonlRequirementReader
 
 __all__ = [
     "AbstractRequirementReader",
